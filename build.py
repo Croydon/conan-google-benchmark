019d7d559453d54b0659401f07333c1652a96069
Croydon <cr0ydon@outlook.com>
2018-02-28 01:43:15 +0100

    Fix shared

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="inexorgame")
     # Try shared and not-shared builds
-    builder.add_common_builds(shared_option_name="c-ares:shared")
+    builder.add_common_builds(shared_option_name="benchmark:shared")
 builder.run()
