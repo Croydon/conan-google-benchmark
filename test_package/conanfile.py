6959eab0fba6dcfd6825ac023a9f466f574a8376
Croydon <cr0ydon@outlook.com>
2018-02-28 21:54:46 +0100

    Fix test_package on macOS

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,10 @@
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
-from conans import ConanFile, CMake
-
-
 class BenchmarkConanPackageTest(ConanFile):
-    settings = {
-        'os': None,
-        'compiler': None,
-        'arch': None,
-        'build_type': ['Release', 'Debug']
-    }
-    generators = 'cmake'
-    build_policy = 'missing'
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    build_policy = "missing"
 
     def build(self):
         cmake = CMake(self)
@@ -19,10 +12,20 @@ class BenchmarkConanPackageTest(ConanFile):
         cmake.build()
 
     def imports(self):
-        for ext in ('.dll', '.pdb'):
-            self.copy(pattern='*{!s}'.format(ext), dst='bin', src='bin')
-        for ext in ('.lib', '.a', '.so*', '.dylib*'):
-            self.copy(pattern='*{!s}'.format(ext), dst='lib', src='lib')
+        for ext in (".dll", ".pdb"):
+            self.copy(pattern="*{!s}".format(ext), dst="bin", src="bin")
+        for ext in (".lib", ".a", ".so*", ".dylib*"):
+            self.copy(pattern="*{!s}".format(ext), dst="lib", src="lib")
+
+    #def test(self):
+        #self.run(os.sep.join([".", "bin", "BenchmarkPackageTest"]))
 
     def test(self):
-        self.run(os.sep.join(['.', 'bin', 'BenchmarkPackageTest']))
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get("DYLD_LIBRARY_PATH", ""), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get("LD_LIBRARY_PATH", ""), bin_path))
